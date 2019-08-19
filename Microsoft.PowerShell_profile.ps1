56bbf4aa3171e5440e7e75eb99a46c51ca420a33
7555G <630r63.7555@gmail.com>
2019-08-19 13:21:29 +0300

    Fix styling and 'ln' arguments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,32 +6,36 @@
 $env:Path = $env:Path + "$HOME\bin"
 
 # set custom prompt
-function prompt {"$(Get-Location | Split-Path -Leaf) "}
+function Prompt {"$(Get-Location | Split-Path -Leaf) "}
 
 # disable default aliases
-remove-item alias:curl # which is "Invoke-WebRequest"
-remove-item -Force alias:diff
+Remove-Item alias:curl # which is "Invoke-WebRequest"
+Remove-Item -Force alias:diff
 
 # use vim to run gvim
 Set-Alias -Name vim -Value gvim
 
 # 'cd' follows ".lnk" shortcuts
-remove-item -force alias:cd
+Remove-Item -Force Alias:cd
 
-function cd($target) {
-    if($target.EndsWith(".lnk")) {
-        $sh = new-object -com wscript.shell
-        $fullpath = resolve-path $target
-        $targetpath = $sh.CreateShortcut($fullpath).TargetPath
-        set-location $targetpath
+Function cd {
+    [CmdletBinding()]
+    param([string][Parameter(Mandatory = $true)]${Target})
+
+    if($Target.EndsWith(".lnk")) {
+        $sh = New-Object -com wscript.shell
+        $Fullpath = Resolve-Path $Target
+        $TargetPath = $sh.CreateShortcut($fullpath).TargetPath
+        Set-Location $TargetPath
     }
     else {
-        set-location $target
+        Set-Location $Target
     }
 }
 
 # 'ln' for powershell
-function ln() {
+function ln {
+    [CmdletBinding()]
     param(
         [string][Parameter(Mandatory = $true, Position = 0)]
         ${ItemType},
@@ -39,25 +43,34 @@ function ln() {
         ${Target},
         [string][Parameter(Mandatory = $true, Position = 2)]
         ${Link},
-        [string[]][Parameter(Position=3, ValueFromRemainingArguments)]
+        [string[]][Parameter(Position = 3, ValueFromRemainingArguments)]
         ${Remaining}
     )
 
     ${LinkPath} = Split-Path -Parent ${Link}
     ${LinkName} = Split-Path -Leaf ${Link}
+    ${NewItemParameters} = @{
+        ItemType = ${ItemType}
+        Path = ${LinkPath}
+        Name = ${LinkName}
+        Value = ${Target}
+    }
 
-    ${Command} = "New-Item -ItemType ${ItemType} -Path ${LinkPath}
-        -name ${LinkName} -value ${Target}
-        ${Remaining}".replace("`r`n", " ")
-    Invoke-Expression ${Command}
+    Invoke-Expression "New-Item @NewItemParameters ${Remaining}"
 }
 
 # 'which' for powershell
-function which(${target}) {
-    Get-Command ${target} -Syntax
+function which {
+    [CmdletBinding()]
+    param([string][Parameter(Mandatory = $true)]${Target})
+
+    Get-Command ${Target} -Syntax
 }
 
 # 'killall' for powershell
-function killall(${target}) {
-    Stop-Process -Name ${target} -Force
+function killall {
+    [CmdletBinding()]
+    param([string][Parameter(Mandatory = $true)]${Target})
+
+    Stop-Process -Name ${Target} -Force
 }
