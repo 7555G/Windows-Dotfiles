f5c34aeb20e333ac164ee3874b19c4b86a19b4c4
7555G <630r63.7555@gmail.com>
2019-08-21 17:04:23 +0300

    Reorganize functions

---
+++ Functions.ps1
@@ -0,0 +1,107 @@
+#
+# ${HOME}\dotfiles\Functions.ps1
+#
+
+# 'cd' that follows ".lnk" shortcuts
+Function cd {
+    [CmdletBinding()]
+    param([Parameter(Mandatory = $true)][string]${Target})
+
+    if (${Target}.EndsWith(".lnk")) {
+        ${shell} = New-Object -com wscript.shell
+        ${Target} = Resolve-Path ${Target}
+        ${TargetPath} = ${shell}.
+                            CreateShortcut(${Target}).
+                            TargetPath
+        Set-Location ${TargetPath}
+    }
+    else {
+        Set-Location ${Target}
+    }
+}
+
+# 'rm' that moves target to Recycle Bin
+function rm {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromRemainingArguments)]
+        [string[]]${Targets}
+    )
+
+    ${Targets} = Resolve-Path ${Targets}
+    ${shell} = New-Object -ComObject 'Shell.Application'
+
+    foreach (${Target} in ${Targets}) {
+        ${shell}.
+            NameSpace(0).
+            ParseName(${Target}).
+            InvokeVerb('Delete')
+
+        if ($?) {
+            "${Target} moved to Recycle Bin"
+        }
+    }
+}
+
+# 'ln' for powershell
+function ln {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]${ItemType},
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]${Target},
+        [Parameter(Mandatory = $true, Position = 2)]
+        [string]${Link},
+        [Parameter(Position = 3, ValueFromRemainingArguments)]
+        [string[]]${Remaining}
+    )
+
+    # check the link is a directory or file
+    if (Test-Path -Path ${Link} -PathType Container) {
+        ${LinkPath} = Resolve-Path ${Link}
+        ${LinkName} = Split-Path ${Target} -Leaf
+    } else {
+        ${LinkPath} = Resolve-Path $( Split-Path ${Link} -Parent )
+        ${LinkName} = Split-Path ${Link} -Leaf
+    }
+    ${Target} = Resolve-Path ${Target}
+
+    ${NewItemParameters} = @{
+        ItemType = ${ItemType}
+        Path = ${LinkPath}
+        Name = ${LinkName}
+        Value = ${Target}
+    }
+
+    #$NewItemParameters
+    Invoke-Expression "New-Item @NewItemParameters ${Remaining}"
+}
+
+# function to set more flexible aliases
+function alias {
+    [CmdletBinding()]
+    param(
+        [string][Parameter(Mandatory = $true, Position = 0)]
+        ${Name},
+        [string][Parameter(Mandatory = $true, Position = 1)]
+        ${Value}
+    )
+
+    ${SetFunction} =
+        "function global:${Name} {
+            [CmdletBinding()]
+            param(
+                [Parameter(ValueFromRemainingArguments)]
+                [string[]]`${Arguments}
+            )
+            
+            if (`${Arguments} -eq `$null) {
+                Invoke-Expression `"${value}`"
+            } else {
+                Invoke-Expression `"${value} `'`${Arguments}`'`"
+            }
+        }"
+
+    Invoke-Expression ${SetFunction}
+}
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,84 +2,23 @@
 # ${PROFILE}
 #
 
+# source script with my functions
+. ~/dotfiles/Functions.ps1
+
 # set $PATH
-$env:Path = $env:Path + "${HOME}\bin"
+$env:Path = $env:Path + ";${HOME}\bin"
 
 # set custom prompt
 function Prompt {"$(Get-Location | Split-Path -Leaf) "}
 
 # disable default aliases
-#Remove-Item alias:curl # which is "Invoke-WebRequest"
-Remove-Item -Force alias:diff
-Remove-Item -Force Alias:cd
+#Remove-Item alias:curl # originally "Invoke-WebRequest"
+Remove-Item alias:diff -Force
+Remove-Item alias:cd
+Remove-Item alias:rm
 
 # aliases
-Set-Alias -Name vim -Value gvim
-Set-Alias -Name ex -Value explorer
-
-# 'cd' follows ".lnk" shortcuts
-Function cd {
-    [CmdletBinding()]
-    param([string][Parameter(Mandatory = $true)]${Target})
-
-    if($Target.EndsWith(".lnk")) {
-        $sh = New-Object -com wscript.shell
-        $Fullpath = Resolve-Path $Target
-        $TargetPath = $sh.CreateShortcut($fullpath).TargetPath
-        Set-Location $TargetPath
-    }
-    else {
-        Set-Location $Target
-    }
-}
-
-# 'ln' for powershell
-function ln {
-    [CmdletBinding()]
-    param(
-        [string][Parameter(Mandatory = $true, Position = 0)]
-        ${ItemType},
-        [string][Parameter(Mandatory = $true, Position = 1)]
-        ${Target},
-        [string][Parameter(Mandatory = $true, Position = 2)]
-        ${Link},
-        [string[]][Parameter(Position = 3, ValueFromRemainingArguments)]
-        ${Remaining}
-    )
-
-    # check the link is a directory or file
-    if (Test-Path -Path ${Link} -PathType Container) {
-        ${LinkPath} = Resolve-Path ${Link}
-        ${LinkName} = Split-Path ${Target} -Leaf
-    } else {
-        ${LinkPath} = Resolve-Path $( Split-Path ${Link} -Parent )
-        ${LinkName} = Split-Path ${Link} -Leaf
-    }
-    ${Target} = Resolve-Path ${Target}
-
-    ${NewItemParameters} = @{
-        ItemType = ${ItemType}
-        Path = ${LinkPath}
-        Name = ${LinkName}
-        Value = ${Target}
-    }
-
-    #$NewItemParameters
-    Invoke-Expression "New-Item @NewItemParameters ${Remaining}"
-}
-
-# 'which' for powershell
-function which {
-    [CmdletBinding()]
-    param([string][Parameter(Mandatory = $true)]${Target})
-
-    Get-Command ${Target} -Syntax
-}
-
-# 'killall' for powershell
-function killall {
-    [CmdletBinding()]
-    param([string][Parameter(Mandatory = $true)]${Target})
-
-    Stop-Process -Name ${Target} -Force
-}
+alias vim     'gvim'
+alias ex      'explorer'
+alias which   'Get-Command -Syntax'
+alias killall 'Stop-Process -Force -Name'
