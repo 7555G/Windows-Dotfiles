3188b26d13aef2c3d9b9939e79fee3d7b3887f02
7555G <630r63.7555@gmail.com>
2019-08-24 01:00:46 +0300

    Clean up

--- reverse_scroll.ps1
+++ reverse_scroll.ps1
@@ -1,30 +1,43 @@
 # This is a powershell script, run with administrator privileges.
 
-# Request Administrative Privileges ########################################
-# according to: https://www.petri.com/run-powershell-scripts-with-administrative-privileges
-param([switch]$Elevated)
+# Request Administrative Privileges
 function Check-Admin {
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    try {
+        ${UserIdentity} =
+            [Security.Principal.WindowsIdentity]::GetCurrent()
+        ${CurrentUser} =
+            New-Object Security.Principal.WindowsPrincipal ${UserIdentity}
+        ${AdminRole} =
+            [Security.Principal.WindowsBuiltinRole]::Administrator
+
+        Return ${CurrentUser}.IsInRole(${AdminRole})
+    } catch {
+        throw "Failed to determine admin privileges. Error '{0}' " -f $_
+    }
 }
+
+# if not running in Admin mode rerun as administrator
 if ((Check-Admin) -eq $false)  {
-if ($elevated)
-{
-# could not elevate, quit
+    ${PowershellArguments} = @{
+        NoProfile = $true
+        NoExit = $true
+        Elevated = $true
+        File = ${PSCommandPath}
+    }
+    ${StartProcesParameters} = @{
+        FilePath = 'powershell.exe'
+        Verb = "RunAs"
+        ArgumentList = ${PowershellArguments}
+    }
+
+    Start-Process @StartProcesParameters
+    exit
 }
- 
-else {
- 
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-exit
-}
-############################################################################
 
 # Reverse mouse wheel vertical scroll FlipFlopWheel = 1
-
 Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
 
 # Reverse mouse wheel horizontal scroll FlipFlopHScroll = 1
-
 Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopHScroll -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopHScroll 1 }
+
+Start-Sleep -Seconds 5
