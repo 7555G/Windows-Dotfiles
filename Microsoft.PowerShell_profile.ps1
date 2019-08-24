9ec211afeb48d27e67ee0ec6a4fc4ac7cb00ca05
7555G <630r63.7555@gmail.com>
2019-08-24 12:22:58 +0300

    Add shortcuts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,10 @@
 # use emacs style readline shortcuts :)
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteChar
+Set-PSReadLineKeyHandler -Key Tab          -Function TabCompleteNext
+Set-PSReadLineKeyHandler -Key Shift+Tab    -Function TabCompletePrevious
+Set-PSReadLineKeyHandler -Key Ctrl+v       -Function Paste
+Set-PSReadLineKeyHandler -Key Ctrl+d       -Function DeleteChar
 Set-PSReadLineKeyHandler -Key Ctrl+Shift+a -Function SelectAll
 
 # source script with my functions
